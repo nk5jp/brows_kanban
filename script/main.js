40a45387984f2d1b07e7aede1e9e1766ff87dfc8
nk5jp <keigo.nagata@gmail.com>
2020-01-04 23:24:06 +0900

    poc of vue.js. this is not completed.

--- script/main.js
+++ script/main.js
@@ -1,25 +1,36 @@
 var offsetX;
 var offsetY;
 
-var onTicketDrag = function(event) {
-    var pageX = event.pageX;
-    var pageY = event.pageY;
+var Ticket = Vue.extend({
+    template: '<div class=ticket @mousedown="onTicketGrasp" @mouseup="onTicketDrop">test</div>', 
+    methods: {
+        onTicketDrag: function(event) {
+            var pageX = event.pageX;
+            var pageY = event.pageY;        
+            event.target.style.left = (pageX - offsetX) + 'px';
+            event.target.style.top = (pageY - offsetY) + 'px';
+        },
+        onTicketDrop: function(event) {
+            event.target.removeEventListener('mousemove', this.onTicketDrag, false);
+            event.target.removeEventListener('mouseout', this.onTicketDrag, false);
+        },
+        onTicketGrasp: function(event) {
+            offsetX = event.offsetX;
+            offsetY = event.offsetY;
+            event.target.addEventListener('mousemove', this.onTicketDrag, false);
+            event.target.addEventListener('mouseout', this.onTicketDrag, false);
+        }
+    }
+});
 
-    event.target.style.left = (pageX - offsetX) + 'px';
-    event.target.style.top = (pageY - offsetY) + 'px';
-}
+new Vue({
+    el: '#app', 
+    components: {
+        'ticket': Ticket
+    }
+});
 
-var onTicketDrop = function(event) {
-    event.target.removeEventListener('mousemove', onTicketDrag, false);
-    event.target.removeEventListener('mouseout', onTicketDrag, false);
-}
-
-var onTicketGrasp = function(event) {
-    offsetX = event.offsetX;
-    offsetY = event.offsetY;
-    event.target.addEventListener('mousemove', onTicketDrag, false);
-    event.target.addEventListener('mouseout', onTicketDrag, false);
-}
+/**
 
 var onButtonClick = function(event) {
     var newTicket = document.createElement('div');
@@ -27,9 +38,7 @@ var onButtonClick = function(event) {
     newTicket.id = '123';
 }
 
-var ticket = document.querySelector('.ticket');
 var button = document.querySelector('.createButton');
 
-ticket.addEventListener('mousedown', onTicketGrasp, false);
-ticket.addEventListener('mouseup', onTicketDrop, false);
 button.addEventListener('click', onButtonClick, false);
+*/
